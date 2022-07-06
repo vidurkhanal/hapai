3c72c0732f52c72f9fc93c11a251f5bd7b3bd856
vidurkhanal <vkhanal@caldwell.edu>
2022-07-06 18:28:08 -0400

    KUBECTL INIT

--- py/src/auth/init_db.py
+++ py/src/auth/init_db.py
@@ -12,22 +12,23 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-cur.execute("DROP TABLE IF EXISTS users;")
-cur.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-cur.execute(
-    "CREATE TABLE users (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,"
-    "username varchar (255) UNIQUE NOT NULL,"
-    "password varchar (255) NOT NULL,"
-    "date_added date DEFAULT CURRENT_TIMESTAMP);"
-)
-hashed_pwd = bcrypt.hashpw(b"ssfsfsf", bcrypt.gensalt())
-cur.execute(
-    "INSERT INTO users (username, password)" "VALUES (%s, %s)",
-    (
-        "vidur@test1.co",
-        hashed_pwd,
-    ),
-)
+
+# cur.execute("DROP TABLE IF EXISTS users;")
+# cur.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+# cur.execute(
+#     "CREATE TABLE users (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,"
+#     "username varchar (255) UNIQUE NOT NULL,"
+#     "password varchar (255) NOT NULL,"
+#     "date_added date DEFAULT CURRENT_TIMESTAMP);"
+# )
+# hashed_pwd = bcrypt.hashpw(b"ssfsfsf", bcrypt.gensalt())
+# cur.execute(
+#     "INSERT INTO users (username, password)" "VALUES (%s, %s)",
+#     (
+#         "vidur@test1.co",
+#         hashed_pwd,
+#     ),
+# )
 
 conn.commit()
 
