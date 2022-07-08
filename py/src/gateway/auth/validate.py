320ac2046c212514f8c020513d19a13c58a2e62f
vidurkhanal <vkhanal@caldwell.edu>
2022-07-07 21:46:29 -0400

    gateway for the minisrv microservice

---
+++ py/src/gateway/auth/validate.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+
+def token(req):
+    if not "Authorization" in req.headers:
+        return None, ("missing credentials,401")
+
+    token = req.headers["Authorization"]
+
+    if not token:
+        return None, ("missing credentials", 401)
+
+    res = requests.post(
+        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/validate", headers={"Authorization": token})
+
+    if res.status_code == 200:
+        return res.txt, None
+    else:
+        return None, (res.txt, res.status_code)
---
+++ py/src/gateway/auth_svc/access.py
@@ -0,0 +1,16 @@
+import os
+from urllib import response
+import requests
+
+
+def login(req):
+    auth = req.authorization
+    if not auth:
+        return None, ("missing credentials", 401)
+    basicAuth = (auth.username, auth.password)
+    res = requests.post(
+        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth=basicAuth)
+    if res.status_code == 200:
+        return response.txt, None
+    else:
+        return None, (res.txt, res.status_code)
