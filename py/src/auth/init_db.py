1249cfbc56cc7dca9f184cddc80b7833ca6e20fb
vidurkhanal <vkhanal@caldwell.edu>
2022-07-13 23:06:58 -0400

    switched from local pg to cloud pg

--- py/src/auth/init_db.py
+++ py/src/auth/init_db.py
@@ -4,31 +4,33 @@ import bcrypt
 
 
 conn = psycopg2.connect(
-    host="localhost",
-    database="minisrv",
-    user=os.environ["DATABASE_USERNAME"],
+    host=os.environ["POSTGRES_HOST"],
+    database=os.environ["DATABASE_NAME"],
+    user=os.environ["DB_USERNAME"],
     password=os.environ["DATABASE_PASSWORD"],
 )
 
 cur = conn.cursor()
 
 
-# cur.execute("DROP TABLE IF EXISTS users;")
-# cur.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-# cur.execute(
-#     "CREATE TABLE users (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,"
-#     "username varchar (255) UNIQUE NOT NULL,"
-#     "password varchar (255) NOT NULL,"
-#     "date_added date DEFAULT CURRENT_TIMESTAMP);"
-# )
-# hashed_pwd = bcrypt.hashpw(b"ssfsfsf", bcrypt.gensalt())
-# cur.execute(
-#     "INSERT INTO users (username, password)" "VALUES (%s, %s)",
-#     (
-#         "vidur@test1.co",
-#         hashed_pwd,
-#     ),
-# )
+cur.execute("DROP TABLE IF EXISTS users;")
+cur.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+cur.execute(
+    "CREATE TABLE users (id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,"
+    "username varchar (255) UNIQUE NOT NULL,"
+    "password varchar (255) NOT NULL,"
+    "date_added date DEFAULT CURRENT_TIMESTAMP);"
+)
+hashed_pwd = bcrypt.hashpw("ssfsfsf".encode("utf-8"), salt=bcrypt.gensalt()).decode(
+    "utf-8"
+)
+cur.execute(
+    "INSERT INTO users (username, password)" "VALUES (%s, %s)",
+    (
+        "vidur@test1.co",
+        hashed_pwd,
+    ),
+)
 
 conn.commit()
 
